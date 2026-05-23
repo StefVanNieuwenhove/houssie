bfc131d37789c79967b2fdd84cccd38e57880987
Stef Van Nieuwenhove <stef.vannieuwenhove@student.hogent.be>
2026-05-23 19:43:12 +0200

    config cors

--- Houssie.Api/Program.cs
+++ Houssie.Api/Program.cs
@@ -13,7 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(
-        policy => policy.WithOrigins("http://localhost:5173")
+        policy => policy.WithOrigins("http://localhost:5173", "https://localhost:5173")
                                         .AllowAnyHeader()
                                         .AllowAnyMethod()
     );
@@ -45,7 +45,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(options => { options.SwaggerEndpoint("/swagger/v1/swagger.json", "Houssie API v1"); });
 }
 
-app.UseCors();
 app.UseHttpsRedirection();
+app.UseCors();
 app.MapControllers();
 app.Run();
